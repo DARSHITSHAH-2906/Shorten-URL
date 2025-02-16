9f1fb9d6dbf185d39679a80a54ee91908f4be594
DARSHITSHAH-2906 <darshitshah501867@gmail.com>
2025-02-16 16:53:40 +0530

    modified

--- connect.js
+++ connect.js
@@ -2,9 +2,17 @@ const mongoose  = require("mongoose");
 require('dotenv').config();
 
 async function ConnecttoDB(){
-    await mongoose.connect(process.env.mongo_url);
-    console.log("MongoDB connected");
+    mongoose.connect(process.env.mongo_url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        tls: true // Ensures secure connection
+    }).then(() => {
+        console.log('MongoDB Connected');
+    }).catch(err => {
+        console.error('MongoDB Connection Error:', err);
+    });
 }
+
 
 module.exports = {
     ConnecttoDB
